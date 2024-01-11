497bca4ac6f38da5598c96ccb6ab5cc1f3a71d61
egiros <dj-g@hotmail.it>
2024-01-11 18:01:38 +0100

    up

--- epibooks/src/__tests__/App.test.js
+++ epibooks/src/__tests__/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "../App";
 
 // 1.Verifica che il componente Welcome venga montato correttamente.
@@ -21,5 +21,67 @@ describe("Main render component test", () => {
     render(<App />);
     const allTheBookCards = screen.getAllByTestId("book-card");
     expect(allTheBookCards).toHaveLegth(150);
+  });
+
+  it("renders CommentArea component", () => {
+    render(<App />);
+    const reviewInputField = screen.getByPlaceholderText(/inserisci qui il testo/i);
+    expect(reviewInputField).toBeInTheDocument();
+  });
+});
+
+describe("filter testing", () => {
+  it("Find just one result for the word 'arrow'", () => {
+    render(<App />);
+    const filterInputField = screen.getByPlaceholderText(/cerca un libro/i);
+    fireEvent.change(filterInputField, { target: { value: "arrows" } });
+    const allTheBookCards = screen.getAllByTestId("book-card");
+    expect(allTheBookCards).toHaveLength(1);
+  });
+
+  it("Find just one result for the word 'witcher'", () => {
+    render(<App />);
+    const filterInputField = screen.getByPlaceholderText(/cerca un libro/i);
+    fireEvent.change(filterInputField, { target: { value: "witcher" } });
+    const allTheBookCards = screen.getAllByTestId("book-card");
+    expect(allTheBookCards).toHaveLength(3);
+  });
+});
+
+describe("SingleBook testing", () => {
+  it("makes book card border change clicking on it", () => {
+    render(<App />);
+    const allTheBookCards = screen.getAllByTestId("book-card");
+    const firstBookCard = allTheBookCards[0];
+    fireEvent.click(firstBookCard);
+    expect(firstBookCard).toHaveStyle("border: 3px solid red");
+  });
+
+  it("restores normal border afrer clicking on a second book", () => {
+    render(<App />);
+    const allTheBookCards = screen.getAllByTestId("book-card");
+    const firstBookCard = allTheBookCards[0];
+    fireEvent.click(firstBookCard);
+    expect(firstBookCard).toHaveStyle("border: 3px solid red");
+    const secondBookCard = allTheBookCards;
+    fireEvent.click(secondBookCard);
+    expect(firstBookCard).toHaveStyle("border: 3px solid red");
+  });
+});
+
+describe("CommentList testing", () => {
+  it("renders no book comments on load", () => {
+    render(<App />);
+    const allTheBookComments = screen.queryAllByTestId("single-comment");
+    expect(allTheBookComments).toHaveLength(0);
+  });
+
+  it("renders comments clicking on a valid book", async () => {
+    render(<App />);
+    const allTheBookCards = screen.getAllByTestId("book-card");
+    const firstBookCard = allTheBookCards[0];
+    fireEvent.click(firstBookCard);
+    const allTheBookComments = await screen.findAllByTestId("single-comment");
+    expect(allTheBookComments).not.toHaveLength(0);
   });
 });
